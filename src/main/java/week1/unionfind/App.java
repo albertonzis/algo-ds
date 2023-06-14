735a96399521505b99813367d49b8aeaba6ad10d
Luiz Alberton <luizalberton@gmail.com>
2023-06-14 14:11:12 -0300

    changes to the tests

--- src/main/java/week1/unionfind/App.java
+++ src/main/java/week1/unionfind/App.java
@@ -6,12 +6,12 @@ import java.util.Random;
 
 public class App {
     
-    static int n = 100000;
+    static int n = 10000000;
 
     public static void main(String[] args) {
         dynamicConnectivity(new QuickFindUF(n));
         dynamicConnectivity(new QuickUnionUF(n));
-        dynamicConnectivity(new QuickUnionWeightedUF(n));
+        dynamicConnectivity(new WeightedQuickUnionUF(n));
     }
 
     private static void dynamicConnectivity(UnionFind dc){
@@ -21,6 +21,7 @@ public class App {
             int r2 = new Random().nextInt(n);
             dc.union(r1, r2);
         }
+
         System.out.printf("%s union time: %s %n", dc.getName(), Duration.between(start, Instant.now()).toMillis());
         start = Instant.now();
         for( int i = 0; i < n; i++) {
@@ -29,5 +30,8 @@ public class App {
             dc.connected(r1, r2);
         }
         System.out.printf("%s connected time: %s %n", dc.getName(), Duration.between(start, Instant.now()).toMillis());
+        start = Instant.now();
+        int find = new Random().nextInt(n);
+        System.out.printf("%s find(%s)=%s time: %s %n", dc.getName(), find, dc.find(find), Duration.between(start, Instant.now()).toMillis());
     }
 }
